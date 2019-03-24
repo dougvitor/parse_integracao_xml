8af92d28f3b529a3b52c2dcdb5d2d5cdb0a49b45
Douglas Vitor <doug.vitor@gmail.com>
2019-03-24 00:30:01 -0300

    parse NF-e utilizando XPath

--- parse_integracao_xml/src/br/com/alura/home/dom/NfeXmlParserDOM.java
+++ parse_integracao_xml/src/br/com/alura/home/dom/NfeXmlParserDOM.java
@@ -1,7 +1,6 @@
 package br.com.alura.home.dom;
 
 import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -10,9 +9,7 @@ import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class NfeXmlParserDOM {
@@ -24,43 +21,38 @@ public class NfeXmlParserDOM {
 		
 		XPath xPath = XPathFactory.newInstance().newXPath();
 		
-		String emitExpression = "/nfeProc/NFe/infNFe/emit";
-		String ideExpression = "/nfeProc/NFe/infNFe/ide";
-		String protNFeExpression = "/nfeProc/protNFe";
+		String emitExpression = "/nfeProc/NFe/infNFe/emit/";
+		String ideExpression = "/nfeProc/NFe/infNFe/ide/";
+		String protNFeExpression = "/nfeProc/protNFe/infProt/";
 		
-		NodeList emit = (NodeList) xPath.compile(emitExpression).evaluate(document, XPathConstants.NODESET);
-		NodeList ide = (NodeList) xPath.compile(ideExpression).evaluate(document, XPathConstants.NODESET);
-		NodeList protNFe = (NodeList) xPath.compile(protNFeExpression).evaluate(document, XPathConstants.NODESET);
-		
-		String cnpj = getElementValue("CNPJ", 0, emit);
-		String cpf = getElementValue("CPF", 0, emit);
-		String nomeFantasia = getElementValue("xFant", 0, emit);
+		String cnpj = getElementValue(document, xPath, emitExpression, "CNPJ");
+		String cpf = getElementValue(document, xPath, emitExpression, "CPF"); 
+		String razaoSocial = getElementValue(document, xPath, emitExpression, "xNome"); 
+		String nomeFantasia = getElementValue(document, xPath, emitExpression,  "xFant"); 
 		System.out.println(cnpj);
 		System.out.println(cpf);
+		System.out.println(razaoSocial);
 		System.out.println(nomeFantasia);
 		
-		String numero = getElementValue("nNF", 0, ide);
-		String serie = getElementValue("serie", 0, ide);
-		String dataEmissao = getElementValue("dhEmi", 0, ide);
+		String numero = getElementValue(document, xPath, ideExpression, "nNF");
+		String serie = getElementValue(document, xPath, ideExpression, "serie");
+		String dataEmissao = getElementValue(document, xPath, ideExpression, "dhEmi");
 		System.out.println(numero);
 		System.out.println(serie);
 		System.out.println(dataEmissao);
 		
-		String chaveNfe = getElementValue("chNFe", 0, protNFe);
-		String situacao = getElementValue("cStat", 0, protNFe);
+		String chaveNfe = getElementValue(document, xPath, protNFeExpression, "chNFe");
+		String situacao = getElementValue(document, xPath, protNFeExpression, "cStat");
 		System.out.println(chaveNfe);
 		System.out.println(situacao);
 		
 	}
 	
-	private static String getElementValue(String name, int index, NodeList nodeList) {
-		Element atual = (Element) nodeList.item(index);
-		NodeList element = atual.getElementsByTagName(name);
-		Node node = element.item(index);
+	private static String getElementValue(Document document, XPath xPath, String expression, String tagName) throws XPathExpressionException {
+		Node node = (Node) xPath.compile(expression.concat(tagName)).evaluate(document, XPathConstants.NODE);
 		if(node != null) {
 			return node.getTextContent();
 		}
-		
 		return null;
 		
 	}
